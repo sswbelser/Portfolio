16833cbdb4bd8c02976e64a17d532c9c0b981a2f
Sabastian <sswbelser@gmail.com>
2015-08-07 14:36:09 -0700

    Major changes

---
+++ scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+  hidePortfolio()
+  hideContact()
+
+  $("#homeButton").on("click", function() {
+    mixpanel.track("Home Button Clicked")
+    hidePortfolio(1000)
+    hideContact(1000)
+    showHome(500)
+  })
+
+  $("#portfolioButton").on("click", function() {
+    mixpanel.track("Portfolio Button Clicked")
+    hideContact(1000)
+    hideHome(1000)
+    showPortfolio(500)
+  })
+
+  $("#contactButton").on("click", function() {
+    mixpanel.track("Contact Button Clicked")
+    hideHome(1000)
+    hidePortfolio(1000)
+    showContact(500)
+  })
+
+  $("#githubIcon").on("click", function() {
+    mixpanel.track("Github Clicked")
+  })
+
+  $("#linkedinIcon").on("click", function() {
+    mixpanel.track("Linkedin Clicked")
+  })  
+
+})
+
+var hideHome = function(speed) {
+  $("#homeSection").hide(speed)
+}
+
+var showHome = function(speed) {
+  $("#homeSection").show(speed)
+}
+
+var hidePortfolio = function(speed) {
+  $("#portfolioSection").hide(speed)
+}
+
+var showPortfolio = function(speed) {
+  $("#portfolioSection").show(speed)
+}
+
+var hideContact = function(speed) {
+  $("#contactSection").hide(speed)
+}
+
+var showContact = function(speed) {
+  $("#contactSection").show(speed)
+}
