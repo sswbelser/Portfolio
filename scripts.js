5789d597048161f22c4c30d9345f497ea1e1ec13
Sabastian <sswbelser@gmail.com>
2015-08-20 13:02:57 -0700

    Added background section and project 2

--- scripts.js
+++ scripts.js
@@ -1,59 +1,63 @@
 $(function() {
 
-  hidePortfolio()
-  hideContact()
+  hidePortfolio();
+  hideContact();
 
   $("#homeButton").on("click", function() {
     mixpanel.track("Home Button Clicked")
     hidePortfolio(1000)
     hideContact(1000)
     showHome(500)
-  })
+  });
 
   $("#portfolioButton").on("click", function() {
     mixpanel.track("Portfolio Button Clicked")
     hideContact(1000)
     hideHome(1000)
     showPortfolio(500)
-  })
+  });
 
   $("#contactButton").on("click", function() {
     mixpanel.track("Contact Button Clicked")
     hideHome(1000)
     hidePortfolio(1000)
     showContact(500)
-  })
+  });
 
   $("#githubIcon").on("click", function() {
-    mixpanel.track("Github Clicked")
-  })
+    mixpanel.track("GitHub Clicked")
+  });
 
   $("#linkedinIcon").on("click", function() {
-    mixpanel.track("Linkedin Clicked")
-  })  
+    mixpanel.track("LinkedIn Clicked")
+  });
+
+  $("#twitterIcon").on("click", function() {
+    mixpanel.track("Twitter Clicked")
+  });
 
 });
 
 var hideHome = function(speed) {
   $("#homeSection").hide(speed)
-}
+};
 
 var showHome = function(speed) {
   $("#homeSection").show(speed)
-}
+};
 
 var hidePortfolio = function(speed) {
   $("#portfolioSection").hide(speed)
-}
+};
 
 var showPortfolio = function(speed) {
   $("#portfolioSection").show(speed)
-}
+};
 
 var hideContact = function(speed) {
   $("#contactSection").hide(speed)
-}
+};
 
 var showContact = function(speed) {
   $("#contactSection").show(speed)
-}
+};
